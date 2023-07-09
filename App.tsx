52954408698d0b021095027c33f33636c737e7e7
antoniovuono <antoniovuono@brq.com>
2023-07-09 19:32:26 -0300

    feat: Font roboto installed and loader to handle the app before fonts loaded configured

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
 import { Groups } from "@screens/Groups";
 import theme from "@theme/index";
 import { ThemeProvider } from "styled-components";
+import { Loading } from "@components/Loading";
+
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Container, Loader } from "./styles";
+
+export const Loading = () => {
+  return (
+    <Container>
+      <Loader />
+    </Container>
+  );
+};
