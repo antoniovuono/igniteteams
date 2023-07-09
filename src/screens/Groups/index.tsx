41255d06e4ede2f86a4a63843a0eb24a88401875
antoniovuono <antoniovuono@brq.com>
2023-07-09 16:44:07 -0300

    chore: Typing theme

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 
-import { Container, Title } from "./styles";
+import { Container } from "./styles";
 
 export const Groups = () => {
-  return (
-    <Container
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-    >
-      <Title>Groups</Title>
-    </Container>
-  );
+  return <Container></Container>;
 };
