962dc7f74a7086f0803dbadd98c470170eced04f
antoniovuono <antoniovuono@brq.com>
2023-07-11 16:05:04 -0300

    feat: List created

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Container } from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
+import { FlatList } from "react-native";
 
 export const Groups = () => {
+  const [groups, setGroups] = useState<string[]>([
+    "Galera da rocketseat",
+    "Galera da BRQ",
+  ]);
+
   return (
     <Container>
       <Header />
 
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
 
-      <GroupCard title="Galera do ingite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 };
