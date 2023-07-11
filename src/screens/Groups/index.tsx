4bddd432fb8ac7703b231fe50ac229ef191d03c8
antoniovuono <antoniovuono@brq.com>
2023-07-11 16:23:53 -0300

    bugfix: Button component created

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,6 +6,7 @@ import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 import { FlatList } from "react-native";
 import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export const Groups = () => {
   const [groups, setGroups] = useState<string[]>([]);
@@ -25,6 +26,8 @@ export const Groups = () => {
           <ListEmpty message="Que tal cadastrar a primeira turma ?" />
         )}
       />
+
+      <Button title="Criar nova turma" />
     </Container>
   );
 };
