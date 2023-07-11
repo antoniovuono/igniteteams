fee5b03588a5f860009b9c77738e43f36650c202
antoniovuono <antoniovuono@brq.com>
2023-07-10 21:57:01 -0300

    feat: Group card component created

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Container } from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
 
 export const Groups = () => {
   return (
@@ -10,6 +11,8 @@ export const Groups = () => {
       <Header />
 
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+
+      <GroupCard title="Galera do ingite" />
     </Container>
   );
 };
