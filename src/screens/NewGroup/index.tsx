3ca61e10bcc5c1d08e897c3ac9da0ae27fe8ca16
antoniovuono <antoniovuono@brq.com>
2023-07-11 22:13:11 -0300

    feat: Input component created

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -4,6 +4,7 @@ import { Container, Content, Icon } from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 
 export const NewGroup = () => {
   return (
@@ -18,7 +19,9 @@ export const NewGroup = () => {
           subtitle="crie a turma para adicionar as pessoas"
         />
 
-        <Button title="Criar" />
+        <Input placeholder="Nome da turma" />
+
+        <Button title="Criar" style={{ marginTop: 20 }} />
       </Content>
     </Container>
   );
